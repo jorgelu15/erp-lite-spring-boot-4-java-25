285f6133b642f70c2590cfb1904d915080e82e87
Jorge <j.guardo.ro@gmail.com>
2026-06-19 10:02:21 -0500

    initializr commit springboot course

---
+++ src/main/java/com/midgar/crm_lite/CrmLiteApplication.java
@@ -0,0 +1,13 @@
+package com.midgar.crm_lite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmLiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrmLiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/midgar/crm_lite/CrmLiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.midgar.crm_lite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmLiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
